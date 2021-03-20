e9c7b3dd4800233333eb900926bffdecabb99ada
dsjuanma <juanmanuel_dasilva@hotmail.com>
2021-03-19 21:19:29 -0300

    Trabajando en ejercicio 05

--- src/guia_01/Hora.java
+++ src/guia_01/Hora.java
@@ -1,6 +1,26 @@
 package guia_01;
 
-
+/****************************************************************
+ *
+ * 5 Modele el objeto Hora, teniendo en cuenta sus atributos, hora, minuto y segundo.
+ *  Tenga en cuenta el rango de valores aceptados para cada uno de estos.
+ *  a. Hora: 0 … 23
+ *  b. Minuto: 0 … 59
+ *  c. Segundo: 0 … 59
+ *  Considere el siguiente comportamiento:
+ *  1. Un método que imprima la hora bajo el siguiente formato hh:mm:ss
+ *  usando zero a la izquierda ejemplo 13:04:22 .
+ *  2. Un método que avance en 1 segundo y devuelva la instancia del objeto.
+ *  3. Un método que retroceda en 1 segundo y devuelva la instancia del objeto.
+ *  4. Instanciar el objeto y probar los métodos creados.
+ *
+ ***************************************************************/
 
 public class Hora {
+
+    //Atributos
+    private int hora;
+    private int min;
+    private int seg;
+
 }
